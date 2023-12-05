4b07e53f981ad2c33c175861b316c09038a63436
landonjett <landonjett@gmail.com>
2023-12-04 18:17:58 -0600

    added basic js, am able to see date at the top of page

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,44 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  // Display the current date in the header of the page
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+
+  // Add a listener for click events on the save button
+  $(".saveBtn").on("click", function () {
+    // Get the id of the time-block containing the clicked button
+    var timeBlockId = $(this).parent().attr("id");
+
+    // Get the user input from the textarea within the same time-block
+    var userInput = $(this).siblings(".description").val();
+
+    // Perform validation - check if the user input is not empty
+    if (userInput.trim() !== "") {
+      // Save the user input in local storage using the time-block id as a key
+      localStorage.setItem(timeBlockId, userInput);
+    } else {
+      // Provide feedback to the user about empty input or handle it in your preferred way
+      alert("Please enter a valid event before saving.");
+    }
+  });
+
+  // Add a listener for click events on the clear button
+  $(".clearBtn").on("click", function () {
+    // Get the parent time-block and clear the associated textarea
+    $(this).siblings(".description").val("");
+    // Clear the stored input from local storage
+    var timeBlockId = $(this).parent().attr("id");
+    localStorage.removeItem(timeBlockId);
+  });
+
+
+
+
+
+
+
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
